28a900069a03b7f514d9bc9040da8111476b0722
Phil Martel <pmartel@alum.mit.edu>
2014-08-07 22:55:34 -0400

    testing
    Changed checkboxes back to buttons, made one background green
    and explored padding the buttons

--- test.py
+++ test.py
@@ -4,16 +4,17 @@ from tkinter import *
 
 class App(Frame):
     """build the basic window frame template"""
-    cb =[]
+    but =[]
     def __init__(self,master):
         super(App,self).__init__(master)
         self.grid()
         self.create_widgets()
+        
     def create_widgets(self):
-        self.cb.append( Checkbutton(self, bg='brown'))
-        self.cb[0].grid(row=1,column=2,sticky=N)
-        self.cb.append( Checkbutton(self, bg='brown'))
-        self.cb[1].grid(row=2,column=1,sticky=N)
+        self.but.append( Button(self, bg='green'))
+        self.but[0].grid(row=1,column=2,pady=2)
+        self.but.append( Button(self, bg='brown'))
+        self.but[1].grid(row=2,column=1,padx=3,pady=2)
         
         
 root = Tk()
