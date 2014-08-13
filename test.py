ded5193454434311dad76237cf9fec0948196fce
Phil Martel <pmartel@alum.mit.edu>
2014-08-12 22:08:26 -0400

    tests working on getting multiple buttons working

--- test.py
+++ test.py
@@ -11,15 +11,26 @@ class App(Frame):
         self.create_widgets()
         
     def create_widgets(self):
-        self.but.append( Button(self, bg='green'))
+        self.but.append( Button(self, bg='green',command = self.pressed))
         self.but[0].grid(row=1,column=2,pady=2)
-        self.but.append( Button(self, bg='brown'))
+        self.but.append( Button(self, bg='brown',command = self.pressed))
         self.but[1].grid(row=2,column=1,padx=3,pady=2)
         
+    def pressed(self):
+        #print( 'button pressed')
+        # this works, try to determine which button see p165 of tkinter.pdf
+        for b in self.but :
+            print(b)
+            print(self)
+            print()
+            pass 
+        pass
         
 root = Tk()
 
 root.title( 'Test window')
 root.geometry('300x100')
 app = App(root)
-app.mainloop()
+#app.mainloop()
+# with the main loop commented, you can loop at app
+# app.__dict__ dumps info about app
