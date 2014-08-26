cbfb160a2575cdf4b84dd3af91a4d3c0b4c9e848
Phil Martel <pmartel@alum.mit.edu>
2014-08-25 22:10:37 -0400

    starting on a 2D array class
    This will become the means of specifying adjacent and jumpTo
    holes

---
+++ TwoD.py
@@ -0,0 +1,14 @@
+# developing code for working with a 2-D array
+class TwoD:
+    arr = []
+    def __init__(self, rows, cols):
+        rObj =[]
+        for r in range(rows):
+            rObj.append([])
+        for c in range(cols):
+            self.arr.append(rObj)
+
+    
+        
+        
+    
