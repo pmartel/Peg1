fb00ca5b50ff0d60dd99304500b1deee491d2e2b
Phil Martel <pmartel@alum.mit.edu>
2014-08-26 22:09:48 -0400

    Trying to get 2d array working.
    It looks like read works but not write.  sheck if we need to work in
    stages.
    
    The array will help set up adjacent and jumpTo lists in PegPuzzle

--- TwoD.py
+++ TwoD.py
@@ -1,14 +1,45 @@
 # developing code for working with a 2-D array
 class TwoD:
-    arr = []
+##    arr = []
+##    cols =0
+##    rows=0
+
     def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.arr = []
         rObj =[]
         for r in range(rows):
             rObj.append([])
         for c in range(cols):
             self.arr.append(rObj)
 
+    def display(self):
+        """ print the 2D array with row and column stuff on the outside"""
+        t = '\t|' # marker
+        print('____',end='')
+        for c in range(self.cols): # column stuff
+            print( t,c, end='')
+        print()
+        for r in range(self.rows):
+            print(r, end='') # row stuff
+            for c in range(self.cols):
+                v = self.arr[c][r]
+                print( t,v, end='')
+            print()
+
+    def seqFill(self):
+        n = 0
+        for r in range(self.rows):
+            for c in range(self.cols):
+                self.arr[c][r] = n
+                n +=1
+
     
-        
-        
+if __name__ == '__main__':
+    a = TwoD(4,3)
+    b = TwoD(3,5)
+    a.seqFill()
+    a.display()
+
     
