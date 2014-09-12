61127e06d56a0319420d149f1024ff202882813f
Phil Martel <pmartel@alum.mit.edu>
2014-09-12 19:22:35 -0400

    This works, but to get to the top of the hierarchy a global
    may be the way to go

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
 ## Phil's utility library
-
+global c1
 class Hierarchy():
     """ this class is set up to help maintain a hierarchy of parent classes
 up to the main application """
@@ -10,11 +10,40 @@ pass that parent class in, otherwise None """
         self.parent = parent
 
     def level(self):
-       count = 0
-       return count
+        """ returns the hierarchy level """
+        count = 0
+        p = self.parent
+        while p != None:
+            p = p.hierarchy.parent
+            count +=1
+        return count
 
-class C1():
-    def __init__(self):
-        hierarchy = Hierarchy(None)
-        c = C2(self)
+    def getRoot(self):
+        """ returns the top level of the hierarchy """
+        root = self
+        p = root.parent
+        
+        while p != None:
+            root = p
+            p = root.hierarchy.parent
+        return root
 
+# debugging code for Hierarchy
+if __name__ != 'main':
+    class C1():
+        def __init__(self):
+            self.hierarchy = Hierarchy(None)
+            self.c2 = C2(self)
+
+    class C2():
+        def __init__(self,parent):
+            self.hierarchy = Hierarchy(parent)
+            if self.hierarchy.level() < 5:
+                self.c2 = C2(self)
+
+        def test(self):
+            print(c1)
+
+    #main routine
+
+    c1 = C1()
